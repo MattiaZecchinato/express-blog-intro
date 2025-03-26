b0a10f554b01bbb5f4ca49a84591bd567fe6fa9e
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-26 14:15:00 +0100

    feat: add image for bacheca

---
+++ posts.js
@@ -0,0 +1,32 @@
+const posts = [
+    {
+        "title": "Ciambellone",
+        "content": "Content for ciambellone",
+        "img": "images/ciambellone.jpeg",
+        "tags": ['dolce', 'farina']
+    },
+    {
+        "title": "Cracker Barbabietola",
+        "content": "Content for cracker barbabietola",
+        "img": "images/cracker_barbabietola.jpeg",
+        "tags": ['cracker', 'salato']
+    },
+    {
+        "title": "Pane Fritto Dolce",
+        "content": "Content for pane fritto dolce",
+        "img": "images/pane_fritto_dolce.jpeg",
+        "tags": ['pane', 'fritto', 'dolce']
+    },
+    {
+        "title": "Pasta Barbabietola",
+        "content": "Content for pasta barbabietola",
+        "img": "images/pasta_barbabietola.jpeg",
+        "tags": ['pasta', 'barbabietola']
+    },
+    {
+        "title": "Torta Paesana",
+        "content": "Content for torta paesana",
+        "img": "images/torta_paesana.jpeg",
+        "tags": ['torta', 'dolce']
+    }
+]

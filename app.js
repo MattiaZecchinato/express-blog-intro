801b4d72fa28b7f5dabe20378361e28183870145
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-03-26 14:02:42 +0100

    feat: add response for route '/'

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// run code inside when client get a request on this specific route
+app.get('/', (req, res) => {
+    
+    console.log('Test');
+    res.send('My blog Server');
+});
+
+
+// listen the port 3000
+app.listen(port, () => {
+
+    console.log(`Listen on port ${port}`);
+});
